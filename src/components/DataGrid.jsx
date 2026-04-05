c496e15eadd2b6e098429725799c5d3a0fe4a80f
bheemreddysakethreddy <sakethbheem@gmail.com>
2026-04-05 12:17:40 +0530

    feat : implemented search by name ,  filter by category functionality and navigation to product page from datagrid

--- src/components/DataGrid.jsx
+++ src/components/DataGrid.jsx
@@ -1,32 +1,61 @@
 import { DataGrid } from "@mui/x-data-grid";
+import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-
 function DataGridPage() {
-
+  const [searchQuery, setSearchQuery] = useState("")
+  const [category, setCategory] = useState("All")
+  const ref = useRef(null)
   const navigate = useNavigate()
-  const rows = [
-    { id: 1, image:"/favicon.svg", name: "Data Grid", description: "the Community version"},
-    { id: 2, image:"/favicon.svg", name: "Data Grid Pro", description: "the Pro version" },
-    { id: 3, image:"/favicon.svg", name: "Data Grid Premium", description: "the Premium version" },
-  ];
+  console.log(category, "category")
+  const rows =
+    [
+      { id: 1, image:"/favicon.svg", name: "Data Grid", description: "the Community version", price:400, category:"fruits"},
+      { id: 2, image:"/favicon.svg", name: "Data Grid Pro", description: "the Pro version", price:100, category:"vegetables" },
+      { id: 3, image:"/favicon.svg", name: "Data Grid Premium", description: "the Premium version", price:500, category:"fruits" },
+    ]
+
   
   const columns = [
     {field:"image"
       , renderCell:(params)=>(
         <img src={params.row.image} alt="" />
-      ),  headerName:"Product Image", width:200},  
-      { field: "name", headerName: "Product Name", width: 200, sortable:false },
-      { field: "description", headerName: "Description", width: 300 },
+      ),  headerName:"Product Image", flex:1},  
+      { field: "name", headerName: "Product Name", flex:2 , sortable:false },
+      { field: "price", headerName: "Price", flex: 1 },
+      { field: "description", headerName: "Description", flex: 3 },
+      { field: "category", headerName: "Category", flex: 2 },
     ];
     
-
+  const[filteredRows,setFilteredRows]=useState(rows) 
+  useEffect(()=>{
+    ref.current = setTimeout(()=>{
+     const result = rows.filter((product)=>(category !== "All" ?product.name.toLowerCase().includes(searchQuery.toLowerCase()) && product.category.toLowerCase().includes(category.toLowerCase()):product.name.toLowerCase().includes(searchQuery.toLowerCase())))
+     setFilteredRows(result)
+    }, 500)
+    return ()=>clearTimeout(ref.current)
+  },[searchQuery, category])
 
   return (
 
-    <div>
-      <div style={{ height: 300, width: '100%' }}>
-          <DataGrid rows={rows} columns={columns} onRowClick={(rows)=>navigate(`/product/${rows.id}`)} className="cursor-pointer"/>
+    <div className="flex flex-col justify-center items-center gap-4">
+      <input type="text" value={searchQuery} onChange={(e)=>setSearchQuery(e.target.value)} className="border outline-none"/>
+      <select onChange={(e)=>setCategory(e.target.value)}>
+        <option value="All" selected>All</option>
+        <option value="fruits">fruits</option>
+        <option value="vegetables">vegetables</option>
+      </select>
+      <div style={{ height: 300, width: '80%' }}>
+          <DataGrid
+            rows={filteredRows}
+            columns={columns}
+            rowHeight={63}
+            onRowClick={(rows)=>navigate(`/product/${rows.id}`, {state:rows.row})}
+            className="cursor-pointer"
+            disableColumnMenu
+            disableColumnResize
+            disableColumnSelector
+          />
       </div>
     </div>
   );
--- src/pages/ProductScreen.jsx
+++ src/pages/ProductScreen.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 function ProductScreen() {
+  const {state} = useLocation()
+  const navigate = useNavigate()
+  const[quantity,setQuantity]=useState(1) 
+  console.log(state)
   return (
-    <div>
-      <h1 className='bg-red-800'>in product page</h1>
+    <div className='flex justify-center items-center flex-col'>
+      <button onClick={()=>navigate("/product")}>Back</button>
+
+      <section className='flex justify-center items-start gap-10'>
+        <div>
+          <img src={state.image} alt="productImage" className='h-100 w-100' />
+        </div>
+        <div className='flex flex-col justify-start'>
+          <p>{state.name}</p>
+          <p>{state.description}</p>
+          <p>Price:{state.price ?? 100}</p>
+          <div className='flex gap-10'>
+            <button className='border px-3' onClick={()=>setQuantity(prev=>prev-1)} disabled={quantity<=1}>-</button>
+          <p>{quantity}</p>
+          <button className='border px-3' onClick={()=>setQuantity(prev=>prev+1)} >+</button>
+          </div>
+        </div>
+      </section>
+
     </div>
   )
 }
